393a496541535d2df4b41eb1cd9b4c3833e0b78c
NetworkBrad <114602912+NetworkBrad@users.noreply.github.com>
2025-06-01 14:05:26 +0100

    Add files via upload

--- pages/login.js
+++ pages/login.js
@@ -16,8 +16,8 @@ export default function Login() {
     <div>
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <button type="submit">Log In</button>
       </form>
       <p>{message}</p>
--- pages/register.js
+++ pages/register.js
@@ -16,8 +16,8 @@ export default function Register() {
     <div>
       <h1>Register</h1>
       <form onSubmit={handleRegister}>
-        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <button type="submit">Register</button>
       </form>
       <p>{message}</p>
